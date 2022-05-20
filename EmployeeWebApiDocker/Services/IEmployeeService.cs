374106df62808d70feb4726e67fa83e02fbec825
deveshrsmishra <105710654+deveshrsmishra@users.noreply.github.com>
2022-05-20 12:33:19 +0530

    Add files via upload

---
+++ EmployeeWebApiDocker/Services/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeWebApiDocker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApiDocker.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployeeById(string employeeId);
+        Task<Employee> UpdateEmployee(Employee employeeObj);
+        Task<Employee> CreateEmployee(Employee employeeObj);
+        Task<Employee> DeleteEmployee(string employeeId);
+    }
+}
